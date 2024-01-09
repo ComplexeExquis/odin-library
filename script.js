cf9f19ad81ef9bd17cc2f849451c4953cb0b2d7c
faishal hanif <budijansen88@gmail.com>
2024-01-09 13:25:04 +0700

    dialog and stuff, need styling and javascript magic to make it work

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+const addBookBtn = document.querySelector("#add-book-btn");
+const dialog = document.querySelector("dialog");
+const closeAddBookBtn = document.querySelector("#close-add-book-btn");
+
+addBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+closeAddBookBtn.addEventListener("click", () => {
+    dialog.close();
+});
+
 const myLibrary = [];
 
-function Book() {
+function Book(name, author, pages, readStatus) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function getUserInputAndCreateBook() {
     
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+    // handle user input
+    
+    
+    const book = new
+    
+    
     myLibrary.push(book);
 }
 
